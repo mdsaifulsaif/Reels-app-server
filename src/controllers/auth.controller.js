c908d53f0fee107b3e52a148875f5f7363cab328
Md Saiful <saifulislam66info@gmail.com>
2025-09-06 00:41:27 +0600

    ceate login api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -44,6 +44,45 @@ async function registerUser(req, res) {
   });
 }
 
+async function loginuser(req, res) {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid email of password",
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message: "Invalid email or Passwrod",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    "4534534jfhjsdjkfhjkshfsdfkjkh"
+  );
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "User logged in successfully",
+    user: {
+      _id: user._id,
+      email: user.email,
+      fullName: user.fullName,
+    },
+  });
+}
+
 module.exports = {
   registerUser,
+  loginuser,
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
 router.post("/register", authController.registerUser);
+router.post("/login", authController.loginuser);
 
 module.exports = router;
