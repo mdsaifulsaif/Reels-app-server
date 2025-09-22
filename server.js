faee28391144b640b95cf0b123db5a58e1ea50c0
Md Saiful <saifulislam66info@gmail.com>
2025-09-23 01:35:07 +0600

    set domain in cors

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-// require("dotenv").config();
-// const app = require("./src/app");
-// const connectDB = require("./src/db/db");
-
-// connectDB();
-
-// app.listen(5000, () => {
-//   console.log("server is running");
-// });
-
 require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
--- src/app.js
+++ src/app.js
@@ -3,14 +3,13 @@ require("dotenv").config();
 var cors = require("cors");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
-// const foodRoutes = require("./routes/food.routes");
 const reelRoutes = require("./routes/reel.routes");
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // তোমার React frontend url
+    origin: ["http://localhost:5173", "https://clipzygo.netlify.app/"],
     credentials: true,
   })
 );
